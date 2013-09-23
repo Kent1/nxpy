69728662fa65ca444c99b7e0654bd51ac87ed04e
Quentin Loos <contact@quentinloos.be>
2013-09-23 10:24:29 +0200

    PEP8 format

--- nxpy/device_proxy.py
+++ nxpy/device_proxy.py
@@ -17,12 +17,18 @@ import re as re_
 import os
 from lxml import etree as etree_
 
-new_ele = lambda tag, attrs={}, **extra: etree_.Element(tag, attrs, **extra)
-sub_ele = lambda parent, tag, attrs={}, **extra: etree_.SubElement(parent, tag, attrs, **extra)
+
+def new_ele(tag, attrs={}, **extra):
+    etree_.Element(tag, attrs, **extra)
+
+
+def sub_ele(parent, tag, attrs={}, **extra):
+    etree_.SubElement(parent, tag, attrs, **extra)
 
 # Globals
 Tag_pattern_ = re_.compile(r'({.*})?(.*)')
 STRING_CLEANUP_PAT = re_.compile(r"[\n\r\s]+")
+
 
 class Device(object):
 
@@ -32,7 +38,6 @@ class Device(object):
         self.interfaces = []
         self.vlans = []
         self.routing_options = []
-
 
     def export(self, netconf_config=False):
         config = new_ele("configuration")
@@ -85,7 +90,7 @@ class Device(object):
             for node in child_:
                 obj_ = Vlan()
                 obj_.build(node)
-                self.vlans.append(obj_) 
+                self.vlans.append(obj_)
         if nodeName_ == 'routing-options':
             for node in child_:
                 childName_ = Tag_pattern_.match(node.tag).groups()[-1]
@@ -93,48 +98,59 @@ class Device(object):
                 if childName_ == 'flow':
                     obj_ = Flow()
                     obj_.build(node)
-                    self.routing_options.append(obj_) 
+                    self.routing_options.append(obj_)
+
 
 class DeviceDiff(Device):
     _instance = None
+
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(DeviceDiff, cls).__new__(
-                                cls, *args, **kwargs)
+            cls._instance = super(
+                DeviceDiff, cls).__new__(cls, *args, **kwargs)
         return cls._instance
 
 devdiff = DeviceDiff()
 
+
 class Interface(object):
+
     def __repr__(self):
         return "Name %s, Description: %s" % (self.name, self.description)
 
-    def __init__(self,name=None,description=None):
+    def __init__(self, name=None, description=None):
         self.name = name
         self.bundle = ''
         self.description = description
         self.vlantagging = ''
         self.tunneldict = []
         # Unit dict is a list of dictionaries containing units to
-        # interfaces, should be index like {'unit': 'name',
-        # 'description': 'foo', 'vlanid': 'bar', 'addresses': ['IPv4addresses', 'IPv6addresses']}
+        # interfaces, should be index like
+        # {
+        # 'unit': 'name',
+        # 'description': 'foo',
+        # 'vlanid': 'bar',
+        # 'addresses': ['IPv4addresses', 'IPv6addresses']
+        # }
         self.unitdict = []
 
-    def get_descr(self): return self.description
-    def set_descr(self,x):
+    def get_descr(self):
+        return self.description
+
+    def set_descr(self, x):
         global devdiff
         self.description = x
         intdiff = Interface(name=self.name, description=self.description)
         if len(devdiff.interfaces) > 0:
-            deviffIntNames = [x.name for x in devdiff.interfaces]
+            deviffIntNames = [i.name for i in devdiff.interfaces]
             if self.name in deviffIntNames:
                 for interface in devdiff.interfaces:
                     if interface.name == self.name:
                         devdiff.interfaces.remove(interface)
         devdiff.interfaces.append(intdiff)
     # The new_description attribute initiates the DeviceDiff class
-    new_description = property(get_descr,set_descr)
-        
+    new_description = property(get_descr, set_descr)
+
     def export(self):
         ifce = new_ele('interface')
         if self.name:
@@ -162,25 +178,28 @@ class Interface(object):
             self.name = name_
         elif nodeName_ == 'description':
             description_ = child_.text
-            description_ = re_.sub(STRING_CLEANUP_PAT, " ", description_).strip()
+            description_ = re_.sub(
+                STRING_CLEANUP_PAT, " ", description_).strip()
             self.description = description_
         elif nodeName_ == 'unit':
             obj_ = Unit()
             obj_.build(child_)
             self.unitdict.append(obj_)
-        
+
+
 class Vlan:
+
     def __repr__(self):
         return "Name %s, Vlan-Id: %s" % (self.name, self.vlan_id)
+
     def __init__(self):
         self.name = ''
         self.vlan_id = ''
         self.operation = None
-                
-    
+
     def export(self):
         if self.operation:
-            vlan = new_ele('vlan', {'operation':self.operation})
+            vlan = new_ele('vlan', {'operation': self.operation})
         else:
             vlan = new_ele('vlan')
         if self.name:
@@ -191,6 +210,7 @@ class Vlan:
             return vlan
         else:
             return False
+
     def build(self, node):
         for child in node:
             nodeName_ = Tag_pattern_.match(child.tag).groups()[-1]
@@ -206,17 +226,22 @@ class Vlan:
             vlanid_ = re_.sub(STRING_CLEANUP_PAT, " ", vlanid_).strip()
             self.vlan_id = vlanid_
 
+
 class Unit:
+
     def __repr__(self):
-        return "Name %s, Description: %s" % (self.name, self.description)    
+        return "Name %s, Description: %s" % (self.name, self.description)
+
     def __init__(self):
         self.name = ''
         self.description = ''
         self.vlan_id = ''
-        #family: {'name':(one of inet, inet6, mpls, iso...), 'addresses':[], 'mtu':'', 'accounting': {}, 'vlan_members':['',''], 'vlan_members_operation':'delete' or 'replace' or 'merge'(this is the default so omit)}
+        # family: {'name':(one of inet, inet6, mpls, iso...), 'addresses':[],
+        # 'mtu':'', 'accounting': {}, 'vlan_members':['',''],
+        # 'vlan_members_operation':'delete' or 'replace' or 'merge'(this is the
+        # default so omit)}
         self.family = []
-        
-        
+
     def export(self):
         unit = new_ele('unit')
         if self.name:
@@ -237,7 +262,7 @@ class Unit:
                     if len(member['addresses']):
                         for address in member['addresses']:
                             addr = new_ele('address')
-                            sub_ele(addr,"name").text = address
+                            sub_ele(addr, "name").text = address
                             mem_name.append(addr)
                 except:
                     pass
@@ -254,13 +279,15 @@ class Unit:
                                 operation = member['vlan_members_operation']
                         except:
                             operation = None
-                        ethernet_switching = sub_ele(family,'ethernet-switching')
+                        ethernet_switching = sub_ele(
+                            family, 'ethernet-switching')
                         vlan = sub_ele(ethernet_switching, 'vlan')
                         for vlan_item in member['vlan_members']:
                             if operation:
-                                vmem = sub_ele(vlan,'members', {'operation':operation})
+                                vmem = sub_ele(
+                                    vlan, 'members', {'operation': operation})
                             else:
-                                vmem = sub_ele(vlan,'members')
+                                vmem = sub_ele(vlan, 'members')
                             vmem.text = vlan_item
                 except:
                     pass
@@ -287,7 +314,8 @@ class Unit:
             self.vlan_id = vlanid_
         elif nodeName_ == 'description':
             description_ = child_.text
-            description_ = re_.sub(STRING_CLEANUP_PAT, " ", description_).strip()
+            description_ = re_.sub(
+                STRING_CLEANUP_PAT, " ", description_).strip()
             self.description = description_
         elif nodeName_ == 'family':
             vlan_unit_list = []
@@ -297,28 +325,33 @@ class Unit:
                 # *************** ETHERNET-SWITCHING ****************
                 if childName_ == 'ethernet-switching':
                     for grandChild_ in node:
-                        grandchildName_ = Tag_pattern_.match(grandChild_.tag).groups()[-1]
+                        grandchildName_ = Tag_pattern_.match(
+                            grandChild_.tag).groups()[-1]
                         if grandchildName_ == 'port-mode':
                             pmode = grandChild_.text
-                            pmode = re_.sub(STRING_CLEANUP_PAT, " ", pmode).strip()
+                            pmode = re_.sub(
+                                STRING_CLEANUP_PAT, " ", pmode).strip()
                             family_dict['port-mode'] = pmode
                         elif grandchildName_ == 'vlan':
                             for vlan_member in grandChild_:
                                 vlanmem = vlan_member.text
-                                vlanmem = re_.sub(STRING_CLEANUP_PAT, " ", vlanmem).strip()
+                                vlanmem = re_.sub(
+                                    STRING_CLEANUP_PAT, " ", vlanmem).strip()
                                 vlan_unit_list.append(vlanmem)
                             family_dict['vlan_members'] = vlan_unit_list
                     self.family.append(family_dict)
 
-class Flow(object):    
+
+class Flow(object):
+
     def __init__(self):
-        self.routes = []        
-        
+        self.routes = []
+
     def export(self):
         flow = new_ele('flow')
         if len(self.routes):
             for route in self.routes:
-                flow.append(route.export()) 
+                flow.append(route.export())
             return flow
         else:
             return False
@@ -329,13 +362,14 @@ class Flow(object):
             self.buildChildren(child, nodeName_)
 
     def buildChildren(self, child_, nodeName_, from_subclass=False):
-            if nodeName_ == 'route':
-                obj_ = Route()
-                obj_.build(child_)
-                self.routes.append(obj_)
+        if nodeName_ == 'route':
+            obj_ = Route()
+            obj_.build(child_)
+            self.routes.append(obj_)
 
 
-class Route(object):    
+class Route(object):
+
     def __init__(self):
         self.name = ''
         self.operation = None
@@ -367,7 +401,7 @@ class Route(object):
             "packet-length": [<numeric-expression(s)>],
             "dscp": [<numeric-expression(s)>],
             "fragment": [
-                "dont-fragment" 
+                "dont-fragment"
                 "not-a-fragment"
                 "is-fragment"
                 "first-fragment"
@@ -394,10 +428,10 @@ class Route(object):
         "routing-instance": "<RouteTarget extended community>"
         }
         '''
-        
+
     def export(self):
         if self.operation:
-           ro = new_ele('route', {'operation':self.operation})
+            ro = new_ele('route', {'operation': self.operation})
         else:
             ro = new_ele('route')
         if self.name:
@@ -406,16 +440,16 @@ class Route(object):
         for key in self.match:
             if self.match[key]:
                 for value in self.match[key]:
-                    sub_ele(match,key).text = value
+                    sub_ele(match, key).text = value
         if match.getchildren():
             ro.append(match)
         then = new_ele("then")
         for key in self.then:
             if self.then[key]:
-                if self.then[key] != True and self.then[key] != False:
-                    sub_ele(then,key).text = self.then[key]
+                if self.then[key] is not True and self.then[key] is not False:
+                    sub_ele(then, key).text = self.then[key]
                 else:
-                    sub_ele(then,key)
+                    sub_ele(then, key)
         if then.getchildren():
             ro.append(then)
         if ro.getchildren():
@@ -435,19 +469,24 @@ class Route(object):
             self.name = name_
         elif nodeName_ == 'match':
             for grandChild_ in child_:
-                grandChildName_ = Tag_pattern_.match(grandChild_.tag).groups()[-1]
+                grandChildName_ = Tag_pattern_.match(
+                    grandChild_.tag).groups()[-1]
                 grandChildText = grandChild_.text
-                grandChildText = re_.sub(STRING_CLEANUP_PAT, " ", grandChildText).strip()
+                grandChildText = re_.sub(
+                    STRING_CLEANUP_PAT, " ", grandChildText).strip()
                 self.match[grandChildName_].append(grandChildText)
         elif nodeName_ == 'then':
             for grandChild_ in child_:
-                grandChildName_ = Tag_pattern_.match(grandChild_.tag).groups()[-1]
+                grandChildName_ = Tag_pattern_.match(
+                    grandChild_.tag).groups()[-1]
                 self.then[grandChildName_] = True
 
+
 class Parser(object):
+
     def __init__(self, confile=None):
         self.confile = confile
-    
+
     def export(self):
         if self.confile:
             confile = self.confile
@@ -465,13 +504,13 @@ class Parser(object):
             kwargs['parser'] = etree_.ETCompatXMLParser()
         doc = etree_.parse(*args, **kwargs)
         return doc
-    
+
     def parse(self):
         '''Normally this would be an rpc_reply in case of netconf invoking or
         a configuration element in case of normal parsing'''
         doc = self.parsexml_(self.confile)
         rootNode = doc.getroot()
-        #NetCONF invoked
+        # NetCONF invoked
         rootNodeTag = Tag_pattern_.match(rootNode.tag).groups()[-1]
         if rootNodeTag == 'rpc-reply':
             rootNode = rootNode.xpath("//*[local-name()='configuration']")[0]
@@ -480,7 +519,7 @@ class Parser(object):
         rootObj = Device()
         rootObj.build(rootNode)
         return rootObj
-    
+
     def parseString(self):
         '''Normally this would be an rpc_reply in case of netconf invoking or
         a configuration element in case of normal parsing'''
@@ -495,4 +534,3 @@ class Parser(object):
         rootObj = Device()
         rootObj.build(rootNode)
         return rootObj
-
